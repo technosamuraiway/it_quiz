47fd8c9033d590a0f14656a9173b7d7282d19307
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2025-02-28 20:02:49 +0300

    feat: QUIZ-3 => app name is changed

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,10 +4,27 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: (
-      <>
-        <header>APP</header>
+      <div style={{ backgroundColor: '#474d47', height: '100vh' }}>
+        <header style={{ textAlign: 'center' }}>header</header>
+
+        <main style={{ marginTop: '50px' }}>
+          <ul
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <li>js</li>
+            <li>ts</li>
+            <li>html</li>
+            <li>react</li>
+            <li>css</li>
+          </ul>
+        </main>
+
         <Outlet />
-      </>
+      </div>
     ),
 
     children: [],
