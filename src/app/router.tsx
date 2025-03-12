dbde5c55b4d61e89b08950bc067725fba755ad14
Pavel <korobovpavel2017@yandex.by>
2025-03-12 11:00:42 +0300

    fixed lint and format code

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,11 @@
-import {createBrowserRouter} from 'react-router-dom'
-import Home from "@/pages/home";
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '@/pages/home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: (
-        <Home/>
+      <Home />
       // <div style={{ backgroundColor: '#474d47', height: '100vh' }}>
       //   <header style={{ textAlign: 'center' }}>header</header>
       //   <Outlet />
